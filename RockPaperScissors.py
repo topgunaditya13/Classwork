88a3fca10927332fc15cae83cd3850c6c27c9694
topgunaditya13 <87445694+topgunaditya13@users.noreply.github.com>
2021-07-16 14:28:47 -0400

    Rock Paper Scissors

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -2,6 +2,22 @@ import random
 def game():
   move=input("Pick rock, paper, or scissors")
   comp_moves=["rock, paper, scissors"]
-  comp_make_a_move = random.choice(comp_moves)
-  if move=comp_make_a_move:
+  comp_move = random.choice(comp_moves)
+  if move=comp_move:
+    print("Tie Game")
+  elif move="rock":
+    if comp_move="paper":
+      print("Computer wins!!!")
+    elif comp_move="scissors":
+      print("Player wins!!!")
+  elif move="paper":
+    if comp_move="scissors":
+      print("Computer wins!!!")
+    elif comp_move="rock":
+      print("Player wins!!!")
+  elif move="scissors":
+    if comp_move="rock":
+      print("Computer wins!!!")
+    elif comp_move="paper":
+      print("Player wins!!!")      
 game()
